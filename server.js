8f10727b25942b107a137ff35a71de3bf788fd0c
Aruwo <mytootoho@gmail.com>
2025-06-13 21:34:36 +0900

    Move public to docs for GitHub Pages

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
 
-require("dotenv").config();
 console.log("Cloudinary Cloud Name:", process.env.CLOUDINARY_CLOUD_NAME);
 console.log("Cloudinary API Key:", process.env.CLOUDINARY_API_KEY);
 console.log("Cloudinary API Secret:", process.env.CLOUDINARY_API_SECRET);
+
+require("dotenv").config();
 const express = require("express");
 const multer = require("multer");
 const { v2: cloudinary } = require("cloudinary");
