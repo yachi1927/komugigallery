9fc6352c3735b24770d0dae5d89afa545d0d3e8a
Aruwo <mytootoho@gmail.com>
2025-06-17 04:01:16 +0900

    Move public to docs for GitHub Pages

--- server.mjs
+++ server.mjs
@@ -11,8 +11,8 @@ import path from "path";
 import dotenv from "dotenv";
 import { fileURLToPath } from "url";
 import cloudinary from "cloudinary";
-import User from "../models/User.js";
-import Post from "../models/Post.js";
+import User from "./models/User.js";
+import Post from "./models/Post.js";
 
 
 // __dirnameを使うための準備 (ESM用)
@@ -350,6 +350,12 @@ async function main() {
           "nekochan",
           "kiroro",
           "hironobu",
+          "hitoya",
+          "kuko",
+          "jyushi",
+          "hainekochan",
+          "toranekochan",
+          "yoshihisa",
         ],
       };
 
