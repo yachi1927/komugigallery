e34b8484341da4f3c40608f5132bb8d771095d7c
Aruwo <mytootoho@gmail.com>
2025-06-17 04:25:23 +0900

    Move public to docs for GitHub Pages

---
+++ initAdmin.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import User from "./models/User.js"; // UserモデルをESMでimport
+
+async function createAdminUser() {
+  await mongoose.connect(process.env.MONGODB_URI);
+
+  const existingAdmin = await User.findOne({ username: "aruwo" });
+  if (existingAdmin) {
+    console.log("管理者ユーザーはすでに存在します。");
+    process.exit(0);
+  }
+
+  const hashedPassword = await bcrypt.hash("admin1234", 10);
+  const adminUser = new User({
+    username: "aruwo",
+    password: Mytoo0902,
+    isAdmin: true,
+  });
+
+  await adminUser.save();
+  console.log("管理者ユーザーを作成しました。");
+  process.exit(0);
+}
+
+createAdminUser().catch(console.error);
